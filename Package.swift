0f16d82c3b83c5833e61a2049adaf56b8d1827a7
Florian Cabot <florian.cabot@etu.unige.ch>
2017-06-10 00:41:25 +0200

    ProofKit is not hosted by me anymore

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "PetriNetLib",
     dependencies: [
-        .Package(url: "https://github.com/Dexter9313/ProofKit",
+        .Package(url: "https://github.com/Mandarancio/ProofKit.git",
                  majorVersion: 0),
     ]
 )
